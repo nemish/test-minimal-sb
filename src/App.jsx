2cb2c95b7b7bbc6ea298653151fc435e0ffabe1f
Iaroslav Melnichuk <nemish.i.nelos@gmail.com>
2024-11-22 17:32:52 +0200

    fix: tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link, useParams } from "react-router-dom";
+
+const SportComponent = () => {
+  const { sportName } = useParams();
+  return <p className="text-3xl">That is {sportName}</p>;
+};
 
 function App() {
   return (
     <div>
       <div>
-        <h1>Minimal sportsbet</h1>
-        <div>
+        <h1 className="text-xl">Minimal sportsbet</h1>
+        <div className="space-x-2">
           <Link to="/">Home</Link>
           <Link to="/sports">Sports</Link>
           <Link to="/sports/soccer">Soccer</Link>
@@ -16,13 +21,13 @@ function App() {
         <Route
           exact
           path="/:lng?/sports"
-          component={() => <p>Hello sports</p>}
+          component={() => <p>This is sports</p>}
         />
-        <Route exact path="/:lng?" component={() => <p>Hello home</p>} />
+        <Route exact path="/:lng?" component={() => <p>Welcome home</p>} />
         <Route
           exact
           path="/:lng?/sports/:sportName"
-          component={() => <p>Hello in sport specific sport</p>}
+          component={SportComponent}
         />
       </Switch>
     </div>
